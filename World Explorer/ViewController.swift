e4bab45478ac3b88f16bc86512674732d839326f
ccebreros1 <ccebreros1@nait.ca>
2017-01-20 10:03:56 -0700

    First commit

---
+++ World Explorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  World Explorer
+//
+//  Created by Cesar A Cebreros Lara on 2017-01-20.
+//  Copyright © 2017 Cesar A Cebreros Lara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
