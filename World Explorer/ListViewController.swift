db727d2831832a03c3bad4016a6398ded636c6d2
ccebreros1 <ccebreros1@nait.ca>
2017-02-09 12:03:16 -0700

    Almost there. Missing unwrapped nil value

--- World Explorer/ListViewController.swift
+++ World Explorer/ListViewController.swift
@@ -18,21 +18,40 @@ class ListViewController:UIViewController, UITableViewDataSource, UITableViewDel
         as! AppDelegate
     
     //Add a var for the event that will ask for permission
-    var eventStore = EKEventStore()
+    let eventStore = EKEventStore()
     var reminders: [EKReminder]!
     var titles : [String] = []
     let cellIdentifier = "reminderCell"
     @IBOutlet weak var remindersTable: UITableView!
-
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+                // Do any additional setup after loading the view.
         remindersTable.dataSource = self
         remindersTable.delegate = self
         remindersTable.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
         askForPermission()
-        getReminders()
+        self.getReminders()
+        
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Ask for reminder permissions
+    func askForPermission(){
+        eventStore.requestAccess(to: EKEntityType.reminder, completion:
+            {(granted, error) in
+                if !granted {
+                    let controller = UIAlertController(title: "Reminder creation failed", message: "Access to reminders not granted", preferredStyle: .alert)
+                    let cancelAction = UIAlertAction(title: "Done", style: .cancel, handler: nil)
+                    controller.addAction(cancelAction)
+                    self.present(controller, animated: true, completion: nil)
+                }
+        })
         
     }
     
@@ -51,37 +70,48 @@ class ListViewController:UIViewController, UITableViewDataSource, UITableViewDel
             cell?.textLabel?.text = titles[indexPath.row]
             return cell!
     }
+    
     func getReminders()
     {
-        let calendars =
-            eventStore.calendars(for: EKEntityType.reminder)
+        let reminder =
+            eventStore.calendars(for: .reminder).first?.calendarIdentifier
         
-        for calendar in calendars as [EKCalendar] {
-            if calendar.title == "Reminders"
+        let reminders = eventStore.calendar(withIdentifier: reminder!)
+        
+        if let remindersUnwrapped = reminders, let reminderUnwrapped = reminder
+        {
+        
+        self.getAllReminders(onCalendar: remindersUnwrapped){ reminders1 in
+            for (i, reminderUnwrapped) in self.reminders.enumerated()
             {
-                for reminder in reminders {
-                    titles.append(reminder.title)
-                }
+                self.titles.append(reminderUnwrapped.title)
             }
+        
+        }
         }
     }
     
-        override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    //Ask for reminder permissions
-    func askForPermission(){
-        eventStore.requestAccess(to: EKEntityType.reminder, completion:
-            {(granted, error) in
-                if !granted {
-                    let controller = UIAlertController(title: "Reminder creation failed", message: "Access to reminders not granted", preferredStyle: .alert)
-                    let cancelAction = UIAlertAction(title: "Done", style: .cancel, handler: nil)
-                    controller.addAction(cancelAction)
-                    self.present(controller, animated: true, completion: nil)
-                }
-        })
+    //Private Functions
+    
+    private func getAllReminders(onCalendar calendar: EKCalendar, completion: @escaping (_ reminders: [EKReminder]) -> Void)
+    {
+        let predicate = eventStore.predicateForReminders(in: [calendar])
+        
+        eventStore.fetchReminders(matching: predicate) {reminders in
+            let reminders1 = reminders?
+                .filter{!$0.isCompleted}
+                .sorted{($0.creationDate ?? Date.distantPast) < ($1.creationDate ?? Date.distantPast)}
+            completion(reminders1 ?? [])
+        }
         
     }
+    
+
+    
+    private func getCalendars() -> [EKCalendar] {
+        return eventStore.calendars(for: .reminder)
+            .filter { $0.allowsContentModifications }
+    }
+    
+    
 }
