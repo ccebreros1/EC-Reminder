9eefdb43893abbe48e594147c6c6b651dd7d5a9e
ccebreros1 <ccebreros1@nait.ca>
2017-02-10 09:32:49 -0700

    Getting the reminders and titles work now!! :smile_cat:

--- World Explorer/ListViewController.swift
+++ World Explorer/ListViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Cesar A Cebreros Lara. All rights reserved.
 //
 //  Followed this tutorial http://sweettutos.com/2015/11/25/eventkit-reminders-manager-how-to-retrieve-create-and-edit-reminders-from-within-your-app-in-swift/ for retrieving a list of reminders from the database of the phone/tablet. Many of the code belongs to that tutorial, and some modifications were made for the purpose of this lab.
+// Also got ideas from https://github.com/keith/reminders-cli/blob/master/Sources/Reminders.swift and many forums of StackOverflow
 
 import UIKit
 import EventKit
@@ -19,19 +20,29 @@ class ListViewController:UIViewController, UITableViewDataSource, UITableViewDel
     
     //Add a var for the event that will ask for permission
     let eventStore = EKEventStore()
+    //Create a variable for an array of reminders
     var reminders: [EKReminder]!
+    //Create a varaible for an array of titles
     var titles : [String] = []
+    //Create a constant for a cell identifiker of the table view
     let cellIdentifier = "reminderCell"
+    //Outlet for the table view
     @IBOutlet weak var remindersTable: UITableView!
    
     override func viewDidLoad() {
         super.viewDidLoad()
-                // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
         remindersTable.dataSource = self
         remindersTable.delegate = self
         remindersTable.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        //Ask for permission
         askForPermission()
-        self.getReminders()
+        //Get all reminders of the device
+        getReminders()
+        //Reload the table view
+        DispatchQueue.main.async{
+            self.remindersTable.reloadData()
+        }
         
     }
     
@@ -42,7 +53,7 @@ class ListViewController:UIViewController, UITableViewDataSource, UITableViewDel
     }
     
     //Ask for reminder permissions
-    func askForPermission(){
+    func askForPermission() -> Void{
         eventStore.requestAccess(to: EKEntityType.reminder, completion:
             {(granted, error) in
                 if !granted {
@@ -54,40 +65,55 @@ class ListViewController:UIViewController, UITableViewDataSource, UITableViewDel
         })
         
     }
-    
+    //SECTION FOR METHODS RELATED TO TABLE VIEW
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Get the number of total rows that the table view should create
         return titles.count
     }
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
         UITableViewCell {
+            //Create a cell
             var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
             if (cell == nil) {
                 cell = UITableViewCell(
                     style: UITableViewCellStyle.default,
                     reuseIdentifier: cellIdentifier)
             }
+            //Add the label of the row
             cell?.textLabel?.text = titles[indexPath.row]
+            //Return a cell that was created
             return cell!
     }
+    //END OF SECTION FOR METHODS RELATED TO TABLE VIEW
     
+    //Get all reminders
     func getReminders()
     {
-        let reminder =
-            eventStore.calendars(for: .reminder).first?.calendarIdentifier
-        
-        let reminders = eventStore.calendar(withIdentifier: reminder!)
-        
-        if let remindersUnwrapped = reminders, let reminderUnwrapped = reminder
-        {
-        
-        self.getAllReminders(onCalendar: remindersUnwrapped){ reminders1 in
-            for (i, reminderUnwrapped) in self.reminders.enumerated()
+        //String for a calendar unique ID
+        var calUID:String! = "?"
+        //Get the reminders calendars array
+        let cal =  eventStore.calendars(for: EKEntityType.reminder) as [EKCalendar]
+        //Loop through the calendars to get the calendar identifier ID
+        for i in cal {
+            if i.title == "Reminders" {
+                //Set the calendar unique ID to the one that was retrieved from the loop
+                calUID = i.calendarIdentifier
+            }
+        }
+        //Create a new instance of the reminders calendar
+        let reminder = eventStore.calendar(withIdentifier: calUID!)
+
+
+        //Get all reminders using the menthod created
+        self.getAllReminders(onCalendar: reminder!){ reminders1 in
+            //Loop through the whole reminders array to get the title of each one
+            for (_, reminder) in reminders1.enumerated()
             {
-                self.titles.append(reminderUnwrapped.title)
+                //add the title to the array of titles
+                self.titles.append(reminder.title)
             }
         
-        }
         }
     }
     
@@ -102,6 +128,8 @@ class ListViewController:UIViewController, UITableViewDataSource, UITableViewDel
                 .filter{!$0.isCompleted}
                 .sorted{($0.creationDate ?? Date.distantPast) < ($1.creationDate ?? Date.distantPast)}
             completion(reminders1 ?? [])
+            //Debugging only
+            print(String(describing: reminders1))
         }
         
     }
