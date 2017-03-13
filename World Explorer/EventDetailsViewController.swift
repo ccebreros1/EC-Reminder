4a7970d884f6d590865f6982e7cf79b5dea94026
ccebreros1 <ccebreros1@nait.ca>
2017-03-13 12:05:07 -0600

    Added table view, is not functional yet

--- World Explorer/EventDetailsViewController.swift
+++ World Explorer/EventDetailsViewController.swift
@@ -6,22 +6,22 @@
 //  Copyright © 2017 Cesar A Cebreros Lara. All rights reserved.
 //
 //The expandable table view was done taking pieces of code from http://www.appcoda.com/expandable-table-view/
-// Added some functionality from the book 
+// Added some functionality from the book
 
 import UIKit
 import EventKit
 import Foundation
 
-class EventDetailsViewController: UIViewController {
+class EventDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     var datePicker: UIDatePicker!
     var reminder: EKReminder!
     var eventStore: EKEventStore!
     var reminderTitle: String?
-    @IBOutlet weak var dateInfoPicker: UIDatePicker!
-    @IBOutlet weak var locationInfoLabel: UILabel!
-
+    var tableViewCell:TableViewCellViewController? = nil
+    let dateCellIdentifier = "dateDueCell"
     //var reminderUrl : URL!
+    @IBOutlet weak var detailsTable: UITableView!
     
     @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
         self.performSegue(withIdentifier: "backToList", sender: self)
@@ -35,11 +35,17 @@ class EventDetailsViewController: UIViewController {
   
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewCell? = TableViewCellViewController()
         titleLabel.text = reminderTitle
-        dateInfoPicker.date = (reminder.dueDateComponents?.date)!
-        locationInfoLabel.text = reminder.location
+        tableViewCell?.datePicker.date = (reminder.dueDateComponents?.date)!
+        tableViewCell?.locationLabel.text = reminder.location
+        //dateCellIdentifier.isHidden = true;
         
         // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+        detailsTable.dataSource = self
+        detailsTable.delegate = self
+        detailsTable.register(UITableViewCell.self, forCellReuseIdentifier: dateCellIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,21 +53,51 @@ class EventDetailsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Educational purposes. This button will go away if the app is published in Appstore since it interferes with apple's policies using undocumented URLStrings
     @IBAction func viewInRemindersApp(_ sender: UIButton) {
         let concatenation : String = remindersUrl + eventId
         let stringUrl: NSString = concatenation as NSString
         let reminderURL = URL(string: stringUrl as String)
         UIApplication.shared.open(reminderURL!, options: [:], completionHandler: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //SECTION FOR METHODS RELATED TO TABLE VIEW
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Get the number of total rows that the table view should create
+        return 3
     }
-    */
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+            //Create a cell
+            var cell = tableView.dequeueReusableCell(withIdentifier: dateCellIdentifier)
+            if (cell == nil) {
+                cell = UITableViewCell(
+                    style: UITableViewCellStyle.default,
+                    reuseIdentifier: dateCellIdentifier)
+            }
+            //Add the label of the row
+            cell?.accessoryType = .detailButton
+            cell?.textLabel?.text = "Hello"
+            //Return a cell that was created
+            return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt
+        indexPath: IndexPath) {
+        if indexPath.section == 0
+        {
+            let cell1 = tableView.dequeueReusableCell(withIdentifier: dateCellIdentifier)
+            cell1?.isHidden = false
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+
+    }
+    
+    
+    //END OF SECTION FOR METHODS RELATED TO TABLE VIEW
+
 
 }
---
+++ World Explorer/TableViewCellViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCellViewController.swift
+//  EC Reminder
+//
+//  Created by Cesar A Cebreros Lara on 2017-03-13.
+//  Copyright © 2017 Cesar A Cebreros Lara. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellViewController: UITableViewCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+}
